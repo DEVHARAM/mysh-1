0624d59b4f38b7b9a9035a4bd5e6390be4da98bc
aeis <gkfka5968@gmail.com>
2017-11-07 16:17:37 +0900

    edit1

--- src/signal.c
+++ src/signal.c
@@ -9,7 +9,7 @@ sigemptyset(&blockset);
 sigaddset(&blockset,SIGINT);
 sigprocmask(SIG_BLOCK,&blockset,NULL);
 
-signal(SIGINT,SIG_IGN);
+//signal(SIGINT,SIG_IGN);
 sigaction(SIGINT,SIG_IGN,NULL);
 pause();
 Stopped(SIGINT);
