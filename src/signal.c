98f7da9d532e009a16d0a258b260b2a3b7d70dd9
aeis <gkfka5968@gmail.com>
2017-11-07 16:45:36 +0900

    sdf

---
+++ src/signal.c
@@ -0,0 +1,27 @@
+#include "signal.h"
+#include <unistd.h>
+#include <signal.h>
+
+void catch_sigint(int)
+{
+sigset_t blockset;
+sigemptyset(&blockset);
+sigaddset(&blockset,SIGINT);
+sigprocmask(SIG_BLOCK,&blockset,NULL);
+
+signal(SIGINT,SIG_IGN);
+sigaction(SIGINT,SIG_IGN,NULL);
+pause();
+Stopped(SIGINT);
+//exit(1);
+}
+void catch_sigtstp(int)
+{
+signal(SIGTSTP,SIG_IGN);
+sigaction(SIGTSTP,SIGSTOP,NULL);
+Stoppend(SIGTSTP);
+pause();
+//exit(0);
+
+
+}
